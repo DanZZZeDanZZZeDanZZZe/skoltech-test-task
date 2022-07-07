d82d18ce11a2ee64bbfae54c6723e0ca6d386a2b
Artyom <jyjyjyj3@gmail.com>
2022-07-07 11:09:42 +0300

    feature: connect to form

--- src/components/FormFieldWrapper/FormFieldWrapper.tsx
+++ src/components/FormFieldWrapper/FormFieldWrapper.tsx
@@ -1,25 +1,25 @@
-import { FormControl, InputLabel } from "@mui/material"
+import { FormControl, InputLabel, Box } from "@mui/material"
 import { cloneElement, useRef } from "react"
-
+import { Control, Controller } from "react-hook-form"
 import { v4 as uuid } from "uuid"
-
-interface FormFieldWrapperProps {
-  label?: string
-  isSelect?: boolean
-  children: JSX.Element
-}
+import { FormFieldWrapperProps } from "../../types"
 
 interface AdditionalProps {
   id: string
   labelid?: string
   margin: "normal"
   label?: string
+  fullWidth: true
+  error: boolean
+  helperText?: string
 }
 
 export const FormFieldWrapper = ({
   label,
   children,
   isSelect,
+  control,
+  rules,
 }: FormFieldWrapperProps): JSX.Element => {
   const idRef = useRef(uuid())
 
@@ -27,22 +27,38 @@ export const FormFieldWrapper = ({
     label,
     id: idRef.current,
     margin: "normal",
+    fullWidth: true,
+    error: false,
   }
 
   if (label) {
     additionalProps.labelid = `label-${idRef.current}`
   }
 
-  const element = cloneElement(children, additionalProps)
-
   return (
-    <FormControl fullWidth sx={{ margin: "1rem" }}>
-      {label && (
-        <InputLabel id={additionalProps.labelid}>
-          {isSelect ? label : null}
-        </InputLabel>
-      )}
-      {element}
-    </FormControl>
+    <Controller
+      control={control}
+      name={additionalProps.id}
+      render={({ field, fieldState }) => {
+        const { error } = fieldState
+
+        if (error) {
+          additionalProps.error = true
+          additionalProps.helperText = error.type
+        }
+
+        return (
+          <>
+            {label && (
+              <InputLabel id={additionalProps.labelid}>
+                {isSelect ? label : null}
+              </InputLabel>
+            )}
+            {cloneElement(children, { ...additionalProps, ...field })}
+          </>
+        )
+      }}
+      rules={rules}
+    />
   )
 }
